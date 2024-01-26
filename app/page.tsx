c0734756107aaa55116c26c7974bca23a6b8300f
Htoomyat Naing <2myat9@gmail.com>
2024-01-26 10:40:07 -0800

    add a button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 export default function Home() {
+  const handleClick = () => {
+    console.log("Hello hello");
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Welcome to Webflow!</div>
+      <div className="text-white">Welcome to Webflow!</div>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Click Me!
+      </button>
     </main>
   );
 }
