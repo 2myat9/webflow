d1b1c4af367580f1339fa65656efc2ace6f64eeb
Htoomyat Naing <2myat9@gmail.com>
2024-02-12 16:08:51 -0800

    set up webflow designer extension to work

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@
 export default function Home() {
   const handleClick = () => {
     console.log("Hello hello");
+    // const myDOMElement = webflow.createDOM("span");
+    // if (myDOMElement.textContent) {
+    //   myDOMElement.setTextContent("Hello, Webflow!");
+    // }
   };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -10,6 +14,7 @@ export default function Home() {
       <button
         type="button"
         className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={handleClick}
       >
         Click Me!
       </button>
