ea1cd8060cac22c8323aaed5b64ba902cc568a59
Amy Liu <amyl20@lakesideschool.org>
2024-02-20 00:20:08 -0800

    generative schema draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,69 @@
 "use client";
 
-import { CopyJSONButton } from "@/CopyJSONButton";
-import copyData from '../copydata.json';
+import { WebflowElementSchema } from './generateTemplate';
+import { generateDOMTree } from './generateTemplate';
+
+
 
 export default function Home() {
-  // const handleClick = async () => {
-  //   const el = await webflow.getAllElements();
-  //   if (el[0].type == "Body") {
-  //     const divEl = webflow.createDOM("div");
-  //     divEl.setTextContent(
-  //       "Hello world!"
-  //     );
-
-  //     let myStyle = await webflow.getStyleByName('divBlockStyle');
-  //     if (!myStyle) {
-  //       myStyle = webflow.createStyle('divBlockStyle');
-        
-  //     }
-  //     myStyle.clearAllProperties();
-  //     myStyle.setProperties({ 'background-color': 'blue', 
-  //                               'font-size': '30px', 
-  //                               'display': 'flex', 
-  //                               'justify-content': 'center'});
-  //     myStyle.save();
-
-  //     divEl.setStyles([myStyle]); 
-      
-  //     const existingChildren = el[0].getChildren();
-  //     el[0].setChildren([...existingChildren, divEl]);
-  //     el[0].save();
-  //   }
-  // };
+  
   const handleClick = async () => {
-    const element = document.getElementById("buttonous");
-    console.log(element);
-    if (!element) return;
+    const element = await webflow.getSelectedElement();
+    let newStyle = await webflow.getStyleByName('beautifulStyle');
+    if (!newStyle) {
+      newStyle = await webflow.createStyle('divBlockStyle');
+      
+    }
+    newStyle.removeAllProperties();
+    newStyle.setProperties({
+    "background-color": "gold",
+    "font-size": "16px",
+    "font-weight": "bold",
+  });
 
-    // Initializing the method
-    new CopyJSONButton({ element, copyData, successText: 'Copied! Paste in Webflow', errorText: 'Didn\'t work' });
+  // Example usage
+  const sampleElement: WebflowElementSchema = {
+    element_type: webflow.elementPresets.DivBlock,
+
+    // content: "Hello, world!",
+    children: [
+        {
+        element_type: webflow.elementPresets.Paragraph,
+        content: "Option 1",
+        styles: newStyle,
+        },
+        {
+          element_type: webflow.elementPresets.Paragraph,
+          content: "Option 2",
+          styles: newStyle,
+        },
+        {
+          element_type: webflow.elementPresets.Paragraph,
+          content: "Option 3",
+          styles: newStyle,
+        },
+    ],
+    
+    };
+
+    if (element && element?.children) {
+      element.prepend(await generateDOMTree(sampleElement, element));
+    
+    } else {
+      console.log("Please select the body element!");
+    }
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-white">Welcome to Webflow!</div>
+      <div className="text-white">Welcome to Webflow! Select the body element from the navigation tree on the left, and click below to get started!</div>
       <button
         id = "buttonous"
         type="button"
         className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
         onClick={handleClick}
       >
-        Click Me!
+        Start Tutorial!
       </button>
     </main>
   );
