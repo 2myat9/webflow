b6caf1d6fb300852e149756a53bebe62efa8b486
Amy Liu <amyl20@lakesideschool.org>
2024-02-26 16:21:30 -0800

    #1 mvp

--- app/checkElementStyles.tsx
+++ app/checkElementStyles.tsx
@@ -1,12 +1,43 @@
 export const checkElementStyles = async () => {
-    const selected = await webflow.getSelectedElement();
-    if (selected && selected.type == "Block") {
-      const styles = await selected.getStyles();
-      if (styles) {
-        const properties = await styles[0].getProperties();
-        console.log("properties => ");
-        console.log(properties);
-        await webflow.notify({type: "Info", message: "sample notification!"})
-      }
+  const body = await webflow.getSelectedElement();
+  if (!body || body.type != "Body") {
+    await webflow.notify({
+      type: "Error",
+      message:
+        "Error. Make sure the Body element is selected before you click Submit",
+    });
+    return;
+  }
+  const children = await body.getChildren();
+  const firstChild = children[0];
+  if (firstChild && firstChild.styles) {
+    const styles = await firstChild.getStyles();
+    if (!styles || styles.length === 0) {
+      await webflow.notify({
+        type: "Error",
+        message: "No styles exist on the selected element",
+      });
     }
+    const properties = await styles[0].getProperties();
+
+    console.log("properties");
+    console.log(properties);
+
+    if (
+      properties &&
+      properties["align-items"] === "center" &&
+      properties["justify-content"] === "center"
+    ) {
+      await webflow.notify({
+        type: "Success",
+        message: "Congrats! You have completed the tutorial.",
+      });
+      // router.push("/tuto");
+    } else {
+      await webflow.notify({
+        type: "Error",
+        message: "Try again!",
+      });
+    }
+  }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,10 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   
   const handleClick = async () => {
-    const router = useRouter();
+    // const router = useRouter();
     const element = await webflow.getSelectedElement();
     const sampleElement = await returnSchema('beautifulStyle');
+    console.log(sampleElement)
     
 
   // Example usage
@@ -27,6 +28,10 @@ export default function Home() {
     }
   };
 
+  const handleCheck = () => {
+    checkElementStyles();
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-white">Welcome to Webflow! Select the body element from the navigation tree on the left, and click below to get started!</div>
@@ -37,6 +42,12 @@ export default function Home() {
         onClick={handleClick}
       >
         Start Tutorial!
+      </button>
+      <button
+      type="button"
+      className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      onClick={handleCheck}>
+        Check
       </button>
     </main>
   );
