ed1767dfc918860bbde93b411e783297f8c4f7c8
Htoomyat Naing <2myat9@gmail.com>
2024-02-29 17:17:02 -0800

    polish up mvp1

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,41 @@
 "use client";
 
-
-import { WebflowElementSchema } from './generateTemplate';
-import { returnSchema } from './templates/firstElement';
-import { generateDOMTree } from './generateTemplate';
-import { checkElementStyles } from './checkElementStyles';
+import { returnSchema } from "./templates/firstElement";
+import { generateDOMTree } from "./generateTemplate";
 import { useRouter } from "next/navigation";
 
-
-
 export default function Home() {
-  
-  const handleClick = async () => {
-    // const router = useRouter();
-    const element = await webflow.getSelectedElement();
-    const sampleElement = await returnSchema('beautifulStyle');
-    console.log(sampleElement)
-    
+  const router = useRouter();
 
-  // Example usage
-  
+  const handleClick = async () => {
+    const element = await webflow.getSelectedElement();
+    const sampleElement = await returnSchema("beautifulStyle");
 
     if (element && element?.children) {
       element.prepend(await generateDOMTree(sampleElement, element));
-    
+      router.push("/tuto");
     } else {
-      console.log("Please select the body element!");
+      await webflow.notify({
+        type: "Error",
+        message:
+          "Please make sure to select the Body element before clicking Start Tutorial",
+      });
     }
-
   };
-
-  const handleCheck = () => {
-    checkElementStyles();
-  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-white">Welcome to Webflow! Select the body element from the navigation tree on the left, and click below to get started!</div>
+      <div className="text-white">
+        Welcome to Webflow! Select the body element from the navigation tree on
+        the left, and click below to get started!
+      </div>
       <button
-        id = "buttonous"
+        id="buttonous"
         type="button"
         className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
         onClick={handleClick}
       >
         Start Tutorial!
-      </button>
-      <button
-      type="button"
-      className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-      onClick={handleCheck}>
-        Check
       </button>
     </main>
   );
--- app/tuto/page.tsx
+++ app/tuto/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import { checkElementStyles } from "../checkElementStyles";
+
 export default function Page() {
-  return <h1>Tutorial 1: Change the Color of a Div</h1>;
+  const handleClick = () => {
+    checkElementStyles();
+  };
+
+  return (
+    <div className="flex flex-col gap-4 p-2">
+      <h1>Tutorial 1:</h1>
+      <p>You are given a flexbox element with 3 children elements inside it.</p>
+      <p>
+        Your goal: Center the children elements on both the horizontal and the
+        vertical axes.
+      </p>
+      <p>
+        Once you are satisfied with the changes you made, select the Body
+        element in the left sidebar and click the Check button below.
+      </p>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={handleClick}
+      >
+        Check
+      </button>
+    </div>
+  );
 }
