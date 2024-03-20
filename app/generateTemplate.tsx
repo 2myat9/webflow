fcd8f74eb2fcdb71d44cb3c88ccaa2f6cb6a608c
Amy Liu <amyl20@lakesideschool.org>
2024-03-19 22:48:49 -0700

    fixed images draft

--- app/generateTemplate.tsx
+++ app/generateTemplate.tsx
@@ -1,5 +1,5 @@
 export type WebflowElementSchema = {
-element_type: ElementPreset<AnyElement>;
+element_type: ElementPreset<AnyElement> | string;   // either the element type OR img src as string
 content?: string | null | undefined;
 children?: WebflowElementSchema[];
 styles?: Style;
@@ -9,24 +9,37 @@ styles?: Style;
 export async function generateDOMTree(schema: WebflowElementSchema, parent: AnyElement): Promise<AnyElement> {
 
     const { element_type, content, children, styles } = schema;
+    let newElement = null;
     if (parent && parent?.children) {
-        const newElement = await parent.append(element_type);
-    
-        if (content && newElement?.textContent) {
-            newElement.setTextContent(content);
+        if (typeof element_type === "string") {
+            newElement = await parent.append(webflow.elementPresets.DOM);
+            newElement.setTag("img");
+            if (newElement?.type === "DOM") {
+                await newElement.setAttribute("src", element_type);
+            }
         }
-        
+        else {
+            newElement = await parent.append(element_type);
+            if (content && newElement?.textContent) {
+                newElement.setTextContent(content);
+            }
+            
+            // Recursively generate and append children
+            if (children && newElement?.children) {
+                for (let childSchema of children) {
+                    newElement.append(await generateDOMTree(childSchema, newElement));
+                }
+            }
+        }
         // Set styles
         if (styles && newElement?.styles) {
+            // let newStyle = await webflow.getStyleByName(styleName);
+            // if (!newStyle) {
+            //     newStyle = await webflow.createStyle("beautifulStyle");
+            // }
             await newElement.setStyles([styles]);
         }
         
-        // Recursively generate and append children
-        if (children && newElement?.children) {
-            for (let childSchema of children) {
-                newElement.append(await generateDOMTree(childSchema, newElement));
-            }
-        }
         return newElement;
     }
     console.log("Error: No parent");
--- app/templates/firstElement.tsx
+++ app/templates/firstElement.tsx
@@ -9,14 +9,14 @@ async function getStyle(styleName: string): Promise<Style> {
   newStyle.setProperties({
     "background-color": "gold",
     "font-size": "16px",
+    "width": "300px",
+    "height": "300px",
     "font-weight": "bold",
   });
   return newStyle;
 }
 
-export async function returnSchema(
-  styleName: string
-): Promise<WebflowElementSchema> {
+export async function returnSchema(styleName: string): Promise<WebflowElementSchema> {
   const newStyle = await getStyle("beautifulStyle");
   return {
     element_type: webflow.elementPresets.HFlex,
@@ -24,17 +24,17 @@ export async function returnSchema(
     // content: "Hello, world!",
     children: [
       {
-        element_type: webflow.elementPresets.Paragraph,
+        element_type: "https://food.fnr.sndimg.com/content/dam/images/food/plus/fullset/2020/04/30/0/FNK_The-Best-Cheddar-And-Herb-Chaffle_H_s4x3.jpg.rend.hgtvcom.1280.1280.suffix/1588257306685.jpeg",
         content: "Option 1",
         styles: newStyle,
       },
       {
-        element_type: webflow.elementPresets.Paragraph,
+        element_type: "https://media.self.com/photos/5f189b76c58e27c99fbef9e3/4:3/w_1131,h_848,c_limit/blackberry-vanilla-french-toast.jpg",
         content: "Option 2",
         styles: newStyle,
       },
       {
-        element_type: webflow.elementPresets.Paragraph,
+        element_type: "https://bestfriendsforfrosting.com/wp-content/uploads/2014/03/blueberry-banana-pancakes.jpg",
         content: "Option 3",
         styles: newStyle,
       },
